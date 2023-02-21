260ed25e9a39880df00cdce102a5a02f61876a50
zhanghuan.odipus <zhanghuan.odipus@bytedance.com>
2023-02-21 10:35:55 +0800

    feat: init
    Change-Id: I5ec8095fb3724436ea6f1733b65ed34c49ce6797

---
+++ client.go
@@ -0,0 +1,102 @@
+package gptclient
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+const (
+	baseURL     = "https://api.openai.com/v1/completions"
+	maxTokens   = 2000
+	temperature = 0.7
+	engine      = "text-davinci-003"
+)
+
+// ChatGPTResponse 返回结构体
+type ChatGPTResponse struct {
+	ID      string                 `json:"id"`
+	Object  string                 `json:"object"`
+	Created int                    `json:"created"`
+	Model   string                 `json:"model"`
+	Choices []Choice               `json:"choices"`
+	Usage   map[string]interface{} `json:"usage"`
+}
+
+type Choice struct {
+	Text         string `json:"text"`
+	Index        int    `json:"index"`
+	LogProbs     int    `json:"logprobs"`
+	FinishReason string `json:"finish_reason"`
+}
+
+// ChatGPTRequest 请求体
+type ChatGPTRequest struct {
+	Model            string  `json:"model"`
+	Prompt           string  `json:"prompt"`
+	MaxTokens        int     `json:"max_tokens"`
+	Temperature      float32 `json:"temperature"`
+	TopP             int     `json:"top_p"`
+	FrequencyPenalty int     `json:"frequency_penalty"`
+	PresencePenalty  int     `json:"presence_penalty"`
+}
+
+func Dialogue(msg, apiKey string) (string, error) {
+	request := ChatGPTRequest{
+		Model:            engine,
+		Prompt:           msg,
+		MaxTokens:        maxTokens,
+		Temperature:      temperature,
+		TopP:             1,
+		FrequencyPenalty: 0,
+		PresencePenalty:  0,
+	}
+
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		return "", err
+	}
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+
+	defer func() {
+		fasthttp.ReleaseResponse(resp)
+		fasthttp.ReleaseRequest(req)
+	}()
+
+	req.SetRequestURI(baseURL)
+	req.SetBody(requestBody)
+	req.Header.SetMethod(fasthttp.MethodPost)
+	req.Header.SetContentType("application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	client := &fasthttp.Client{}
+
+	if err := client.DoTimeout(req, resp, 60*time.Second); err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode() != fasthttp.StatusOK {
+		return "", err
+	}
+
+	response := &ChatGPTResponse{}
+	if err := json.Unmarshal(resp.Body(), response); err != nil {
+		return "", err
+	}
+
+	var reply string
+
+	if response.Choices != nil && len(response.Choices) > 0 {
+		reply = response.Choices[0].Text
+	}
+
+	log.Printf("response text: %s", reply)
+
+	return reply, nil
+}
+
+
